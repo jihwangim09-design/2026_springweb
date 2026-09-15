47c19d4c6ebd29e92cadc631ebedb4776318f88e
jihwangim09-design <jihwangim09@gmail.com>
2026-09-15 12:12:58 +0900

    0915 1212

--- src/main/java/example/Practice7/model/Entity/BoradEntity.java
+++ src/main/java/example/Practice7/model/Entity/BoradEntity.java
@@ -1,10 +1,6 @@
 package example.Practice7.model.Entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import example.Practice6.BaseTime;
-import jakarta.persistence.CascadeType;
+import example.Practice7.BaseTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,11 +14,11 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity @Table ( name = "board")
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
-public class BoardEntity extends BaseTime {
+@Data @NoArgsConstructor  @AllArgsConstructor @Builder 
+public class BoradEntity extends BaseTime{
     @Id 
-    @GeneratedValue ( strategy = GenerationType.IDENTITY )
-    private Integer Id;
+    @GeneratedValue ( strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String author;
     private String password;
     private String content;
@@ -30,5 +26,5 @@ public class BoardEntity extends BaseTime {
     @OneToMany ( mappedBy = "boardEntity" , cascade = CascadeType.ALL)
     @ToString.Exclude
     @Builder.Default
-    private List<CommentEntity> commentEntities = new ArrayList<>();
+    
 }
