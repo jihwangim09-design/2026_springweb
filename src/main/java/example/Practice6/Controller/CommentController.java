80bb5d587dee526eafe7ca1f7f00db03b9a5c8aa
jihwangim09-design <jihwangim09@gmail.com>
2026-09-16 01:43:11 +0900

    0916 0143

--- src/main/java/example/Practice6/Controller/CommentController.java
+++ src/main/java/example/Practice6/Controller/CommentController.java
@@ -15,14 +15,14 @@ import example.Practice6.Dto.CommentDto;
 public class CommentController {
     @Autowired CommentService commentService;
 
-    @PostMapping ("/api/board/comments")
+    @PostMapping ("/api/board/comments") // /api/comment라고 하면 댓글이 독립적인 존재처럼 보여서 게시글이랑 관련있는지 URL만 봐서 알기 어려줌
     public boolean 댓글등록( @RequestBody CommentDto commentDTo){
         return commentService.댓글등록( commentDTo );
 
     }
     @DeleteMapping ("/api/board/comments")
     public boolean 댓글삭제(
-        @RequestParam ( name = "commentId") Integer commentId,
+        @RequestParam ( name = "commentId") Integer commentId, // 게시글id 인지 댓글 id인지 구분이 안되서 comment라고 구분
         @RequestParam ( name = "password") String password) {
         return commentService.댓글삭제( commentId , password );
     }
--- src/main/java/example/Practice7/Controller/CommentController.java
+++ src/main/java/example/Practice7/Controller/CommentController.java
@@ -1,31 +1,29 @@
 package example.Practice7.Controller;
 
-import example.Practice7.BaseTime;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Entity // JPA가 관리하는 진짜 DB 테이블용 클래스
-@Table ( name = "comment") // 이 클래스는 DB의 comment라는 이름의 테이블이랑 연결
-@Data @NoArgsConstructor @AllArgsConstructor @Builder // Lombok , Lombok은 반복적으로 써야 하는 코드(getter, setter, 생성자 등)를 자동으로 만들어주는 도구
-public class CommentController extends BaseTime{
-    @Id // PK(기본키)
-    @GeneratedValue ( strategy = GenerationType.IDENTITY ) // MYSQL의 AUTO_INCREMENT와 같은 개념
-    private Integer id; // 필드선언
-    private String author; // 필드선언
-    private String password; // 필드선언
-    private String content; // 필드선언
+import example.Practice7.Service.CommentService;
+import example.Practice7.dto.CommentDto;
 
-    @ManyToOne
-    @JoinColumn ( name = "board_id")
-    
+@RestController 
+public class CommentController{
+    @Autowired CommentService commentService;
 
+    @PostMapping ("/api/comment")
+    public boolean 댓글등록( @RequestBody CommentDto commentDto){
+        return commentService.댓글등록( commentDto );
+    }
+
+    @DeleteMapping ("/api/comment")
+    public boolean 댓글삭제( 
+        @RequestParam ( name = "id") Integer id,
+        @RequestParam ( name = "password") String password 
+    ){
+        return commentService.댓글삭제(id, password);
+    }
 }
