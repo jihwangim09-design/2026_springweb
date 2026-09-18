0146c1114a98a43d4ffb56d9edf25945df413137
jihwangim09-design <jihwangim09@gmail.com>
2026-09-18 10:20:27 +0900

    0918 1020

---
+++ src/main/java/example/day08/ApiService.java
@@ -0,0 +1,11 @@
+package example.day08;
+
+public class ApiService {
+    
+}
+
+/*
+
+
+
+*/
---
+++ src/main/java/example/day08/AppStart.java
@@ -0,0 +1,13 @@
+package example.day08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication 
+public class AppStart {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStart.class);
+    }
+}
+// implementation 'org.springframework.boot:spring-boot-starter-webflux'
