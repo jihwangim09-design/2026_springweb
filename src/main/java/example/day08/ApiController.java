a971d3e8a060d62fbe925ec6e1cb5e45b7337c5b
jihwangim09-design <jihwangim09@gmail.com>
2026-09-18 10:40:27 +0900

    0918 1040

--- src/main/java/example/day08/ApiController.java
+++ src/main/java/example/day08/ApiController.java
@@ -1,5 +1,19 @@
 package example.day08;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController @RequiredArgsConstructor 
 public class ApiController {
-    
+    private final ApiService apiService;
+
+    // 1.
+    @GetMapping ("/test1") 
+    public Map<String,Object> test1(){
+        return apiService.test1();
+    }
 }
--- src/main/java/example/day08/ApiService.java
+++ src/main/java/example/day08/ApiService.java
@@ -1,11 +1,18 @@
 package example.day08;
 
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service 
 public class ApiService {
     
+
+    // 1.
+    public Map<String,Object> test1(){
+        // 1. API 주소( 공공데이터 신청한 api 요청 url )
+        String url ="https://api.odcloud.kr/api/15103411/v1/uddi:efd2cc22-353c-47f0-83e5-abc6dca54f6f?page=1&perPage=10&serviceKey=98c12c038483f278cb5fff938472af4d61d4565864258f12f9308cd04217863b";
+
+        
+    }
 }
-
-/*
-
-
-
-*/
